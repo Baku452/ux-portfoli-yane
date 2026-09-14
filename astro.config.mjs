93d9d645d647c01c3587eb6bf2235baf264d2511
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2026-09-14 13:25:00 -0500

    Fix(-): layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   fonts: [{
+    provider: fontProviders.local(),
+    name: "Baloo2",
+    cssVariable: "--font-distant-galaxy",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/Baloo2-VariableFont_wght.woff2'],
+        weight: 'normal',
+        style: 'normal'
+      }]
+    }
+  }]
+});
